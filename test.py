0e5a8f92431197940e669d1b9aa2f17ccbed1fa6
Yoel <s73017@beuth-hochschule.de>
2023-07-13 14:42:11 +0200

    multiple images test

--- test.py
+++ test.py
@@ -83,37 +83,39 @@ class HumanGreeterModule(ALModule):
         colorSpace = 11   # RGB
 
         videoClient = camProxy.subscribe("python_client", resolution, colorSpace, 5)
+        
+        for i in range(0,5):
+            t0 = time.time()
 
-        t0 = time.time()
+            # Get a camera image.
+            # image[6] contains the image data passed as an array of ASCII chars.
+            naoImage = camProxy.getImageRemote(videoClient)
 
-        # Get a camera image.
-        # image[6] contains the image data passed as an array of ASCII chars.
-        naoImage = camProxy.getImageRemote(videoClient)
+            t1 = time.time()
 
-        t1 = time.time()
+            # Time the image transfer.
+            print("acquisition delay ", t1 - t0)
 
-        # Time the image transfer.
-        print("acquisition delay ", t1 - t0)
+            camProxy.unsubscribe(videoClient)
 
-        camProxy.unsubscribe(videoClient)
+            ## Now we work with the image returned and save it as a PNG  using ImageDraw
+            # package.
 
-        ## Now we work with the image returned and save it as a PNG  using ImageDraw
-        # package.
+            # Get the image size and pixel array.
+            imageWidth = naoImage[0]
+            imageHeight = naoImage[1]
+            print(imageHeight)
+            array = naoImage[6]
+            print(array)
 
-        # Get the image size and pixel array.
-        imageWidth = naoImage[0]
-        imageHeight = naoImage[1]
-        print(imageHeight)
-        array = naoImage[6]
-        print(array)
+            # Create a PIL Image from our pixel array.
+            im = Image.frombytes("RGB", (imageWidth, imageHeight), array)
 
-        # Create a PIL Image from our pixel array.
-        im = Image.frombytes("RGB", (imageWidth, imageHeight), array)
+            # Save the image.
+            im.save("images/camImage"+str(imageWidth+imageHeight)+str(t0)+".png", "PNG")
 
-        # Save the image.
-        im.save("images/camImage"+str(imageWidth+imageHeight)+".png", "PNG")
-
-        im.show()
+            im.show()
+            time.sleep(5)
 
 
 def main():
